4c5e1653508600385f22a0a7f2e44fc1aa5ea51a
Scapigliata <ScapigliataLudwig@users.noreply.github.com>
2019-07-09 18:35:28 +0200

    Die Project Test

---
+++ app.js
@@ -0,0 +1,10 @@
+
+function myfunction(){
+    document.querySelector("#die-cast1").innerHTML = random();
+    document.querySelector("#die-cast2").innerHTML = random();
+}
+
+function random() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
