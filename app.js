49abd56ddcc9cdc5e871dbd73602886a359819e9
Scapigliata <ScapigliataLudwig@users.noreply.github.com>
2019-07-09 18:56:43 +0200

    Die Project Commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 function myfunction(){
     document.querySelector("#die-cast1").innerHTML = random();
     document.querySelector("#die-cast2").innerHTML = random();
-}
+} 
 
 function random() {
     return Math.floor(Math.random() * 6) + 1;
